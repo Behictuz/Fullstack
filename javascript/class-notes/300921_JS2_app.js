8c31172a028968ccf6bc95a92344051568fa5660
Behictuz <behictuz@gmail.com>
2021-10-04 21:55:40 +0200

    new commit

---
+++ javascript/class-notes/300921_JS2_app.js
@@ -0,0 +1,33 @@
+console.log("I 'm from outside...")
+var quaty=3;
+console.log(quaty);
+var a=5;
+console.log(a);
+
+
+var alert=5;
+console.log(alert);
+console.log(typeof alert);
+
+// var a =10;
+// console.log(" a baslangic : ",a);
+
+// {
+//     var b =5;
+//     console.log("scope icinde b : " ,  b)
+// }
+
+// let a =23;
+// console.log(a);
+
+// let a=11;
+// console.log(a);
+
+// const x="Hello";  //Buraya atadigimiz degeri degistiremiyoruz
+// console.log(x);
+// const x ="Hello World"; 
+
+
+var x = 0.1+0.2;
+
+console.log(+x.toFixed(2));
