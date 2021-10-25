47b1cfc6e7801c49a76aa3b738dacdd010c706b5
Behictuz <behictuz@gmail.com>
2021-10-25 22:02:09 +0200

    newcommit2

---
+++ javascript/class-notes/061021_JS4_app.js
@@ -0,0 +1,31 @@
+// let score=52;
+// let grade= score >= 50;
+// console.log(grade);
+
+// if (score>=80) {
+//             console.log(`bu süper bir haber!! notunuz ${score}`);
+
+// }else if (score>=50) {
+//     console.log("iyi mutluyuz");
+// }
+
+
+//  else{
+//         console.log("dersten kaldiniz");
+// }
+
+//Ternary If else
+// let score =50;
+
+// score >=50 ? console.log(`bu süper bir haber!! notunuz ${score}`):
+//              console.log("üzgünüz kaldiniz");
+
+// let username;
+
+// username ? console.log(`Merhaba ${username}` ): console.log("please Login");
+
+// let score =prompt("Sinav notunu giriniz: ");
+// score>=80 ? console.log("Tebrikler notunuz gecerli cok iyisiniz"):
+// (score>=50 ? console.log("Tebrikler sinavdan gectiniz"):
+// console.log("sinavdan kaldiniz"));
+
