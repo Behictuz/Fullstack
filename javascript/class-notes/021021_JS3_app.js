6181347fdd6783b05b006a91b62654f3c3202d9c
Behictuz <behictuz@gmail.com>
2021-10-25 21:30:57 +0200

    newcommit

---
+++ javascript/class-notes/021021_JS3_app.js
@@ -0,0 +1,86 @@
+console.log("Hello world");
+// var a=5;
+// var b=10;
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// var c = 100+50*3;
+// console.log(c);
+
+// a= ++b;
+// a= b++;
+// console.log({a});
+// console.log({b});
+
+// var d =a/b;
+// console.log(typeof d, d);
+// var e=7;
+// var f=2;
+// console.log(f*=e);
+// console.log(f);
+// console.log(e);
+// console.log(f);
+// g=10/3;
+// console.log(parseInt(g)); Sadece int istiyorsak bunu kullaniriz
+
+// var a=10;
+// var b= '2';
+// var c=2;
+
+// c= a+b;
+// console.log(c , typeof c);
+
+// d= +c; tipini number yapar
+// console.log(typeof d);
+// console.log(typeof(a/b),a/b);
+// console.log(typeof(a*b),a*b); burda islemi number olarak yapar
+
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(b==c);
+// console.log(b===c);
+
+// console.log(a>b);
+// console.log('1'.charCodeAt());
+// console.log('a'.charCodeAt());
+// console.log('fulLstack'< 'fullstack');
+
+
+// console.log(015+025);   // 34   5+8 =13 -- 5+16=21
+//Octal sayi sistemi 0,1,2,3,4,5,6,7
+// console.log(088+25);
+
+// console.log(null+3);
+// console.log(false<true);
+
+// && and || or ! not
+
+// console.log('Boolean("0") equals True' ,Boolean("0")==true);
+// console.log(null==true);
+// console.log(null==false);//null hep null
+// console.log(undefined==true); //undefined = false
+// console.log(Boolean(0)==Boolean(null));
+
+// var a;
+// while(isNaN(a)){
+//     a=prompt("Enter number");
+//     a=Number(a);
+//     console.log(a,typeof(a));
+// }
+
+// console.log(false&&true);
+// console.log(false||true);
+
+// console.log(3&&4);
+// console.log(0&&4);
+// console.log(0||4);
+
+//COALESCING OPerator ilk deger null or undefined ise direkt 2. degeri alir
+
+// console.log(null ?? true);
+
+
+
+
