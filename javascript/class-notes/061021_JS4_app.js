ff9be06fae4fe6926aa0a4bc364f66006a25927d
Behictuz <behictuz@gmail.com>
2021-10-27 21:05:57 +0200

    newcommit

--- javascript/class-notes/061021_JS4_app.js
+++ javascript/class-notes/061021_JS4_app.js
@@ -29,3 +29,75 @@
 // (score>=50 ? console.log("Tebrikler sinavdan gectiniz"):
 // console.log("sinavdan kaldiniz"));
 
+
+// var product="elma"
+// var cost;
+// switch(product){
+//     case "elma":
+//         cost="Fiyat 3$";
+//         break;
+//     case "armut":
+//         cost="Fiyat 5$";
+//         break;
+//     default:
+//         cost="Yok";
+//         break;
+// }
+// console.log(cost);
+
+// var day= prompt("Bir gün girin :");
+// switch(day){
+//     case "pazar":
+//     case "cuma":
+//     case "persembe":
+//     case "sali":
+//         console.log("Workday");
+//         break;
+//     case "pazartesi":
+//         console.log("teamwork");
+//         break;
+//     case "carsamba":
+//         console.log("teamwork");
+//         break;
+//     default:
+//         console.log("Tatil");
+// }
+
+// let password ="asdmiwqdimwqidm";
+// if(password.length >=5 && password.includes("@")){
+//     console.log("that password is strong");
+// }
+// else{
+//     console.log("password is not long enough");
+// }
+
+// let i =0;
+// let sum=0;
+
+// while(i<11){
+//     sum +=i;
+//     console.log(sum);
+//     i++;
+// }
+// console.log({i});
+
+// let text="Clarusway"
+// var newtext=''
+
+// for(let i=8;i>-1;i--){
+//     newtext+=text[i];
+//     console.log(newtext);
+    
+// }
+
+
+// var sayi=prompt("Bir sayi giriniz:");
+
+// if (sayi==2  ) {
+//         console.log("sayi asaldir.")}
+// else if(sayi%2!=0 && sayi%3!=0 && sayi%5!=0 && sayi%7!=0 ){
+//     console.log("sayi asildir..")
+// }
+// else{
+//     console.log("Sayi normaldir.. ")
+// }
