86438366905a6cb491e4006553b6d813ad54f079
Behictuz <behictuz@gmail.com>
2021-11-03 20:15:08 +0100

    commit2

---
+++ javascript/class-notes/111021_JS6_Method.js
@@ -0,0 +1,67 @@
+// var str1="hello";
+// console.log(str1);
+// var str2=new String("Hello World");
+// console.log(str2);
+
+// concat()
+
+
+// var str1="Behic";
+// var str2="Gizem";
+
+// var str3= str1.concat(str2);
+// console.log(str3);
+
+//     CharAt()   default ilk elemani getirir.
+
+
+// var best= "Behic Tüz";
+
+// var last= best.charAt();
+// console.log(last);
+
+
+//           includes()
+
+// var best= "Behic Tüz";
+
+// var last= best.includes("ic T");
+// console.log(last);
+
+
+//            indexof()
+
+
+// var best= "Behic Tüz";
+
+// var last= best.indexOf("i");
+// console.log(last);
+
+
+// Example
+
+// function kac(str,a){
+//     for (let i = 0; i < str.length; i++) {
+//         const element = str[i];
+//         if(element==a){
+//             console.log(i,element);
+        
+//     }
+    
+//     }
+// }
+// kac("hamet naber naasilsin","a");
+
+//       search()
+
+// var str1="Hey adams What are you doing?";
+
+// var str2= str1.search("adams");
+// console.log(str2);
+
+
+// Fahrenheit celcius change
+
+//1.Users give c>f or f>c
+//2.Exit 
+
